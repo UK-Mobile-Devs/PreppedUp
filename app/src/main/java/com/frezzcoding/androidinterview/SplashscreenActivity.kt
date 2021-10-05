06d35af249196f63d9177349c834565d0e0419dc
oskarlasota <frezzyysmite@gmail.com>
2021-10-05 11:35:24 +0100

    replacing deprecated code

--- app/src/main/java/com/frezzcoding/androidinterview/SplashscreenActivity.kt
+++ app/src/main/java/com/frezzcoding/androidinterview/SplashscreenActivity.kt
@@ -1,8 +1,12 @@
 package com.frezzcoding.androidinterview
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import kotlinx.android.synthetic.main.fragment_splashscreen_activity.*
 
 class SplashscreenActivity : AppCompatActivity() {
@@ -12,7 +16,7 @@ class SplashscreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_splashscreen_activity)
         iv_note.alpha = 0f
-        iv_note.animate().setDuration(1500).alpha(1f).withEndAction(){
+        iv_note.animate().setDuration(1500).alpha(1f).withEndAction{
             val i = Intent(this,MainActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
@@ -24,5 +28,19 @@ class SplashscreenActivity : AppCompatActivity() {
         super.onResume()
     }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUIAndNavigation(this)
+        }
+    }
+
+    private fun hideSystemUIAndNavigation(activity: Activity) {
+        val decorView: View = activity.window.decorView
+        WindowInsetsControllerCompat(window, decorView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.hide(WindowInsetsCompat.Type.navigationBars())
+        }
+    }
 
 }
