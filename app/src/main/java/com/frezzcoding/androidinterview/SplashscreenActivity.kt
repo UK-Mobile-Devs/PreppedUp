34acc9d42c94629d32859d7a5c0e960ea544ecd3
oskarlasota <frezzyysmite@gmail.com>
2021-10-05 15:02:12 +0100

    added a motionlayout for the splashscreen

--- app/src/main/java/com/frezzcoding/androidinterview/SplashscreenActivity.kt
+++ app/src/main/java/com/frezzcoding/androidinterview/SplashscreenActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import kotlinx.android.synthetic.main.fragment_splashscreen_activity.*
@@ -15,14 +16,31 @@ class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_splashscreen_activity)
+
+        /*
         iv_note.alpha = 0f
-        iv_note.animate().setDuration(1500).alpha(1f).withEndAction{
-            val i = Intent(this,MainActivity::class.java)
+        iv_note.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
         }
+         */
+
+        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                val i = Intent(this@SplashscreenActivity, MainActivity::class.java)
+                startActivity(i)
+            }
+
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {}
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {}
+
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {}
+        })
     }
+
 
     override fun onResume() {
         super.onResume()
