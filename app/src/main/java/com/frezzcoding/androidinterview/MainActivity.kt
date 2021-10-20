180494df6334af2fc6ca2f55309be19874b5923d
Inveniumm <u.denli@reply.com>
2021-10-20 16:49:04 +0100

    Added changes and fixes

--- app/src/main/java/com/frezzcoding/androidinterview/MainActivity.kt
+++ app/src/main/java/com/frezzcoding/androidinterview/MainActivity.kt
@@ -2,9 +2,11 @@ package com.frezzcoding.androidinterview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,6 +15,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private var firstTime: Boolean? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +24,17 @@ class MainActivity : AppCompatActivity() {
         authenticate()
         registerObserver()
         setupViews()
+
+        val PREFS_NAME = "MyPrefsFile"
+
+        val settings = getSharedPreferences(PREFS_NAME, 0)
+
+        if (settings.getBoolean("first_time", true)) {
+            // first time task
+            firstTimerun()
+            // record the fact that the app has been started at least once (Edit the key if you need to check first time again)
+            settings.edit().putBoolean("first_time", false).commit()
+        }
     }
 
     override fun onResume() {
@@ -29,6 +43,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun authenticate(){
 
+    }
+
+    private fun firstTimerun(){
+
+        // Function gets called only for first run
+        Log.d("Comments", "First time")
+        setContentView(R.layout.content_intro)
+        navController = Navigation.findNavController(
+                this,
+                R.id.nav_host_fragment2
+        )
     }
 
     private fun setupViews(){
