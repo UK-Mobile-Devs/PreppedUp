f03cabbccbf029661e795bbe54c9e9dad907da94
oskarlasota <frezzyysmite@gmail.com>
2021-10-24 01:55:47 +0100

    storing data when introduction is complete

--- app/src/main/java/com/frezzcoding/androidinterview/util/DataStorePreferences.kt
+++ app/src/main/java/com/frezzcoding/androidinterview/util/DataStorePreferences.kt
@@ -2,22 +2,24 @@ package com.frezzcoding.androidinterview.util
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.dataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.map
 
-class DataStorePreferences(val context : Context) {
+class DataStorePreferences(val context: Context) {
 
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
-        name = "INTRO_DATASTORE"
-    )
 
-    private val INTRO_COMPLETED = booleanPreferencesKey("introCompleted")
+    companion object {
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+            name = "INTRO_DATASTORE"
+        )
 
-    suspend fun saveIntroStatusToDataStore(completed : Boolean) {
+        private val INTRO_COMPLETED = booleanPreferencesKey("introCompleted")
+    }
+
+    suspend fun saveIntroStatusToDataStore(completed: Boolean) {
         context.dataStore.edit { preferences ->
             preferences[INTRO_COMPLETED] = completed
         }
