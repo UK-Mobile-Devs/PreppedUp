e5d2b00fdfab13621f38d4562b712217e570f99b
oskarlasota <frezzyysmite@gmail.com>
2021-10-25 22:54:06 +0100

    setting up intro screen test

---
+++ app/src/androidTest/java/com/frezzcoding/androidinterview/IntroScreenTest.kt
@@ -0,0 +1,23 @@
+package com.frezzcoding.androidinterview
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.frezzcoding.androidinterview.util.DataStorePreferences
+import org.junit.Before
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class IntroScreenTest {
+
+    lateinit var mockDataStorePreferences: DataStorePreferences
+
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+        mockDataStorePreferences = DataStorePreferences(ApplicationProvider.getApplicationContext())
+    }
+
+
+}
